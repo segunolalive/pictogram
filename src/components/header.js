99b3829303c2b6c0f2db6e5d6500a4584eb8d757
segun ola <segunlalive@gmail.com>
2017-06-03 18:00:48 +0100

    minor UI improvements

--- src/components/header.js
+++ src/components/header.js
@@ -1,36 +1,11 @@
 import React, { Component } from 'react';
+import './header.css';
 
-
-const style = {
-  width: '100%',
-  textAlign: 'center',
-  color: '#ffffff',
-  backgroundColor: '#044f62',
-  position: 'fixed',
-  top: 0,
-  margin: '0 0 1px',
-  padding: '5px',
-  letterSpacing: '5px',
-  boxShadow: '2px 3px 5px 1px rgba(0, 0, 0, 0.2)',
-  zIndex: 1000,
-}
-
-const toggleStyles = {
-  backgroundColor: '#fff',
-  border: '1px solid #ddd',
-  borderRadius: '3px',
-  display: 'inline-block',
-  position: 'absolute',
-  bottom: '5px',
-  left: '5px',
-  color: '#044f62',
-
-}
 
 class Header extends Component {
   render () {
     return (
-      <header style={style}>
+      <header className='header'>
         <h1>
           PICTOGRAM
         </h1>
@@ -40,5 +15,3 @@ class Header extends Component {
 }
 
 export default Header;
-
-// <span className='toggle-filters'>Filters</span>
